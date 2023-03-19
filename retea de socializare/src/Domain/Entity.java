7b8dbbe6f702fc4ac59d8b0000e5ece26ee2ecd8
Croitor_Sebastian <91545845+Sebic29@users.noreply.github.com>
2023-03-19 16:47:48 +0200

    Add files via upload

---
+++ retea de socializare/src/Domain/Entity.java
@@ -0,0 +1,19 @@
+package Domain;
+
+public class Entity<ID> {
+    private ID id;
+
+    public ID getID() {
+        return id;
+    }
+
+    public void setID(ID id) {
+        this.id = id;
+    }
+
+    public Entity(ID id) {
+        this.id = id;
+    }
+
+
+}
